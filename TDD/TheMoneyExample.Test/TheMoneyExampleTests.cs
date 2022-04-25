01b158f3459b185267811f5399691b009411a454
rbbarad <rbbarad@amazon.com>
2022-04-24 23:53:59 -0400

    create checklist. start small. do bolded item. strikethrough when completed

---
+++ TDD/TheMoneyExample.Test/TheMoneyExampleTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TheMoneyExample.Test
+{
+    public class TheMoneyExampleTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TDD/TheMoneyExample.md
@@ -0,0 +1,6 @@
+# Part 1: The Money Example
+
+## Checklist
+
+$5 + 10 CHF = $10 if rate is 2:1
+**$5 * 2 = $10**
